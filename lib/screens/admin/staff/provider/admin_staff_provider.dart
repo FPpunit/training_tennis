57b365d90c70262c7687e229bb13f5a2985e9c04
Punit Lohani <77602170+punitlohani8@users.noreply.github.com>
2023-09-11 18:31:16 +0530

    Adding Api in Admin Staff Screen

--- lib/screens/admin/staff/provider/admin_staff_provider.dart
+++ lib/screens/admin/staff/provider/admin_staff_provider.dart
@@ -1,42 +1,109 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 class AdminStaffProvider extends ChangeNotifier{
 
-  List<Map<String,dynamic>> contactData = [
-    {
-      'name' : 'Punit Lohani',
-      'phoneNumber' : '9876543210',
-      'email': 'punitlohani@gmail.com'
-    },{
-      'name' : 'Anny H',
-      'phoneNumber' : '9876543210',
-      'email': 'punitlohani@gmail.com'
-    },
-    {
-      'name' : 'Anny ',
-      'phoneNumber' : '9876543210',
-      'email': 'punitlohani@gmail.com'
-    },
-  ];
+  List<dynamic> contactData = [];
+  static const String token ='626|KHon3CLdfvbK3ElMQSrXdO7Qpbglewn5Q9aG150z';
 
-  addOrUpdateStaff({required bool isDetails,int? index,required String name,required String phone,required String email,}){
-    if(isDetails){
-      contactData[index!]['name'] = name;
-      contactData[index!]['phoneNumber'] = phone;
-      contactData[index!]['email'] = email;
+  getStaffListData() async {
+
+    final uri =Uri.parse("https://api.tenniskhelo.com/api/academy/staff-list");
+    final response = await http.get(
+        uri,
+        headers: {
+      'Accept' : 'application/json',
+      'Authorization' : 'Bearer $token'
+    });
+    if(response.statusCode == 200){
+      final json = jsonDecode(response.body) as Map;
+      contactData = json['staff list'];
+      print(contactData);
     }else{
-      contactData.add({
-        'name' : name,
-        'phoneNumber' : phone,
-        'email' : email,
-      });
+
     }
     notifyListeners();
   }
 
+  createStaff({required Map body}) async {
+
+    final uri =Uri.parse("https://api.tenniskhelo.com/api/academy/create-staff");
+    final response = await http.post(
+        uri,
+        headers: {
+      'Accept' : 'application/json',
+      'Authorization' : 'Bearer $token'
+    },
+    body: body
+    );
+
+    if(response.statusCode == 200){
+      getStaffListData();
+    }else{
+      print('Error ----------------------------------------------------------------------------->');
+    }
+
+  }
+
+  updateStaffDetails({required Map body,required int id}) async {
+    final uri =Uri.parse("https://api.tenniskhelo.com/api/academy/staff-update/$id");
+    final response = await http.post(
+        uri,
+        headers: {
+      'Accept' : 'application/json',
+      'Authorization' : 'Bearer $token'
+    },
+    body: body
+    );
+
+    if(response.statusCode == 200){
+      getStaffListData();
+    }else{
+      print('Error ----------------------------------------------------------------------------->');
+    }
+  }
+
+  deleteStaff({required int id}) async {
+
+    final uri =Uri.parse("https://api.tenniskhelo.com/api/academy/staff-delete/$id");
+    final response = await http.post(
+        uri,
+        headers: {
+          'Accept' : 'application/json',
+          'Authorization' : 'Bearer $token'
+        },
+
+    );
+
+    if(response.statusCode == 200){
+      getStaffListData();
+    }else{
+      print('Error ----------------------------------------------------------------------------->');
+    }
+  }
+  
+
+
+  addOrUpdateStaff({required bool isDetails,int? index,required String name,required String phone,required String email,}){
+    if(isDetails){
+      updateStaffDetails(body: {
+        'name' : name,
+        'phone' : phone,
+        'email' : email,
+      }, id: contactData[index!]['id']);
+    }else{
+      createStaff(body: {
+        'name' : name,
+        'phone' : phone,
+        'email' : email,
+      });
+    }
+  }
+
   removeStaff({required int index}){
-    contactData.removeAt(index);
-    notifyListeners();
+    deleteStaff(id: contactData[index]['id']);
   }
 
 }
